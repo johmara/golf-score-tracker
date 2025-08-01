d500f866b9d235dc6938b14539450fae423a9b87
JohanM <johanm@centiro.se>
2025-08-01 23:18:03 +0200

    create components

---
+++ client/src/app/scorecard/scorecard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scorecard',
+  imports: [],
+  templateUrl: './scorecard.component.html',
+  styleUrl: './scorecard.component.scss'
+})
+export class ScorecardComponent {
+
+}
